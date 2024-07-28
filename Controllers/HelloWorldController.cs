ce6312846070ffff1a6348f1cfb50a893e286bf8
Hdsgf <zubelevichivan@gmail.com>
2024-07-28 15:35:52 +0300

    Index for HelloWorldController

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -6,15 +6,17 @@ namespace MvcMovie.Controllers;
 public class HelloWorldController : Controller 
 {
 
+    //
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "this is my default action...";
+        return View();
     }
 
+    //
     //GET: /HelloWorld/Welcome/
-    public string Welcome(string name, int numTimes = 1)
+    public string Welcome(string name, int ID = 1)
     {
-    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
     }
 }
